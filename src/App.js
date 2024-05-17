c70c6d0b9f493ffce0ba19f1afd15cbfffbd4ddc
Buildar <projetobuildar@gmail.com>
2024-05-16 22:18:27 -0300

    esqueleto landing page
    apenas divisão, nada foi feito ainda

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id='primeiraTela'>
+        <p>Primeira Tela: apresentação</p>
+      </div>
+      <div id='segundaTela'>
+        <p>Segunda Tela: informação da gamificação</p>
+      </div>
+      <div id='terceiraTela'>
+        <p>Terceira Tela: informação das skins</p>
+      </div>
+      <div id='quartaTela'>
+        <p>Quarta Tela: informação da gratuidade</p>
+      </div>
+      <div id='quintaTela'>
+        <p>Quinta Tela: informação da assinatura</p>
+      </div>
+      <div id='sextaTela'>
+        <p>Sexta Tela: informação da aba de networking</p>
+      </div>
+      <div id='setimaTela'>
+        <p>Sétima Tela: depoimentos</p>
+      </div>
+      <div id='oitavaTela'>
+        <p>Oitava Tela: footer</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
