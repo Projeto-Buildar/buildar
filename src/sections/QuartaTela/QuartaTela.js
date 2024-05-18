090823315c19f3cffa39e87c1991618c2c8aba81
Erik Paulino <153562789+ErikPaulino87@users.noreply.github.com>
2024-05-17 23:14:28 -0300

    Alterações

--- src/sections/QuartaTela/QuartaTela.js
+++ src/sections/QuartaTela/QuartaTela.js
@@ -1,7 +1,21 @@
-export default function PrimeiraTela() {
-  return(
-    <main id="quartaTela">
-      <p>Quarta Tela: informação da gratuidade</p>
-    </main>
+import React from 'react';
+import './QuartaTela.css';
+import capiquarta from './img/capiquarta.png'
+
+const QuartaTela = () => {
+  return (
+    <div className="quarta-tela">
+      <div className="text-container">
+        <h1 className="title">
+          Uma plataforma acessível ao <span className="purple-text">Jovem</span>!
+        </h1>
+        <p className="description">
+          Pagar pra aprender? Só se quiser! Aqui contamos com uma opção <span className="blue-text">100% gratuita</span> para quem estiver afim.
+        </p>
+      </div>
+      <img src={capiquarta} alt="Capiquarta" className="capiquarta-image" />
+    </div>
   );
-}
+};
+
+export default QuartaTela;
