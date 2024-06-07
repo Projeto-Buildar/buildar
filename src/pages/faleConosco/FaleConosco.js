cce5ab265268ae2773b9901344c1971af971187a
Erik Paulino <153562789+ErikPaulino87@users.noreply.github.com>
2024-06-06 23:10:22 -0300

    Add arquivos do fale conosco

--- src/pages/faleConosco/FaleConosco.js
+++ src/pages/faleConosco/FaleConosco.js
@@ -1,7 +1,37 @@
-export default function FaleConosco (){
+import React from "react";
+import "./FaleConosco.css";
+import logo from "./images/Logomarca Oficial.png";
+import circuloesquerdo from "./images/circuloesquerdo.png";
+import circulodireito from "./images/circulodireito.png"; 
+
+export default function FaleConosco() {
     return (
-        <main>
-            <h1>Formulário de contato</h1>
-        </main>
-    )
+        
+        <div className="App">
+            <img src={logo} alt="Logo" className="logo_img" />
+            <img src={circuloesquerdo} alt="Círculo Esquerdo" className="circulo-esquerdo" />
+        
+                
+                <h1>Fale Conosco</h1>
+                <h2>Qual seria sua dúvida?</h2>
+                <form className="contact-form">
+                    <div className="form-group">
+                        <input type="text" name="nome" placeholder="Nome completo" required />
+                    </div>
+                    <div className="form-group">
+                        <input type="email" name="email" placeholder="Email" required />
+                        <input type="tel" name="telefone" placeholder="Telefone" required />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="assunto" placeholder="Assunto" required />
+                    </div>
+                    <div className="form-group">
+                        <textarea name="mensagem" rows="5" placeholder="Mensagem" required></textarea>
+                    </div>
+                    <button type="submit">Entrar</button>
+                </form>
+        
+            <img src={circulodireito} alt="Círculo Direito" className="circulo-direito" />
+        </div>
+    );
 }
