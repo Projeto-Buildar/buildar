a01b59fd4fe7e10ce6620183cd123a0dd7b0112d
Isaque <161786728+IsaqueSouzaa@users.noreply.github.com>
2024-06-13 17:21:23 -0300

    atualização do header da home

---
+++ src/home/Banner.js
@@ -0,0 +1,7 @@
+export default function Banner() {
+    return(
+        <div>
+            <h1>Para qual andar hoje?</h1>
+        </div>
+    );
+}
---
+++ src/home/HeaderHome.js
@@ -0,0 +1,50 @@
+import './headerHome.css'
+import logo from './images/logo_on.png';
+import BtnsNav from './BtnsNav';
+import { Outlet, Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
+import "../../i18n"
+
+export default function HeaderHome() {
+  const {t, i18n } = useTranslation();
+  const textoBtn = [
+    {
+      texto: "Home",
+      linkado: "/home"
+    },
+    {
+      texto: t("Connect"),
+      linkado: "/conexoes"
+    },
+    {
+      texto: t("Shop"),
+      //linkado: "/loja"
+    },
+    {
+      texto: t("Profile"),
+      linkado: "/perfil"
+    },
+    {
+      texto: t("voluntary"),
+       linkado: "/inscricaoVoluntario"
+    },
+    {
+      texto: t("ranked"),
+       linkado: "/ranqueada"
+    }
+  ]
+
+  return (
+    <header>
+      <nav>
+      <Link to="/">
+        <a href='./index.html'><img src={logo} className='logo' /></a>
+      </Link>
+      {textoBtn.map((object) => (          
+          <BtnsNav texto={object.texto} link={object.linkado}></BtnsNav>        
+        ))}
+      </nav>
+      <Outlet/>
+    </header>
+  );
+}
