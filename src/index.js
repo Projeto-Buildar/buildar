99bbf2399f23a3a878bc0f3bee7841fa3751beb6
Edugomes12 <gfeduardo730@gmail.com>
2024-06-18 01:22:25 -0300

    Vlibras atualizado

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import VLibras from './Vlibras';
 
-const loadVLibras = () => {
-  const script = document.createElement('script');
-  script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
-  script.async = true;
-  document.body.appendChild(script);
-};
+// const loadVLibras = () => {
+//   const script = document.createElement('script');
+//   script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+//   script.async = true;
+//   document.body.appendChild(script);
+// };
 
-loadVLibras();
+// loadVLibras();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <App />
-    <VLibras />
+    {/* <VLibras /> */}
   </React.StrictMode>
 );
 
