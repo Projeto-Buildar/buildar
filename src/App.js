dbe150e947c91b687f46252ff9489490fc6a4781
Christian Rui <chris2w0social@gmail.com>
2024-06-29 14:49:30 -0300

    Arrumando bandeiras na linguagens

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ export default function App() {
 
       <div className='languageButton'>
         <select name="language" id="language" aria-placeholder='Idioma' onChange={handleLanguageChange} value={selectedLanguage}>
-          <option value="pt">&#x1F1E7;&#x1F1F7;</option>
+          <option value="pt">🇧🇷</option>
           <option value="en">🇺🇸</option>
           <option value="es">🇧🇴</option>
         </select>
