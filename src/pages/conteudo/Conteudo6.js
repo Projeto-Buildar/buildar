15c85337b70e08d0ee83638758d2234964e72da6
Christian Rui <chris2w0social@gmail.com>
2024-06-30 12:45:26 -0300

    adicionando horarios ao conteúdo

--- src/pages/conteudo/Conteudo6.js
+++ src/pages/conteudo/Conteudo6.js
@@ -5,10 +5,22 @@ import DroppableArea from './DroppableArea';
 import { ItemProvider } from './Itens'; // Importa o ItemProvider do contexto
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useEffect } from 'react';
 
 const listaHorarios = ['9h', '10h', '11h', '12h', '13h', '14h', '15h']
 
 export default function Conteudo() {
+
+
+//  let tagsInicias = [];
+//   useEffect(() => {
+//     tagsInicias = listaHorarios.map((horario, index) => 
+//       {
+//         console.log(index)
+//         return <DroppableArea id={index +1} className={`droppable-area-${index+1} droppable-area`} />
+//       });
+//   }, []);
+    
   return (
     <ItemProvider> {/* Envolve o conteúdo com o ItemProvider */}
       <DndProvider backend={HTML5Backend}>
@@ -59,30 +71,8 @@ export default function Conteudo() {
             <p>Planejamento das tarefas</p>
             <div className='atividade'>
               {/* <div className='alinhamento_drop3'> */}
-              <div className='drop3'>
-                {/* 
-            {listaHorarios.map((horario, index) => (
-                  <div>
-                    <p style={{ textAlign: "center" }} key={index}>{horario}</p>
-                    <DroppableArea id={index} className={`droppable-area-${index} droppable-area`} />
-
-                  </div>
-
-                ))} */}
-                <DroppableArea id="8" className="droppable-area-8" />
-                <DroppableArea id="9" className="droppable-area-9" />
-                <DroppableArea id="10" className="droppable-area-10" />
-                <DroppableArea id="11" className="droppable-area-11" />
-                <DroppableArea id="12" className="droppable-area-12" />
-                <DroppableArea id="13" className="droppable-area-13" />
-                <DroppableArea id="14" className="droppable-area-14" />
-              </div>
-
-              {/* </div> */}
-              {/* 
-          <div className='buttonreset'> */}
-              {/* <div className='alinhamento_drop'> */}
               <div className='drop1'>
+              {/* {tagsInicias} */}
                 <DroppableArea id="1" className="droppable-area-1" />
                 <DroppableArea id="2" className="droppable-area-2" />
                 <DroppableArea id="3" className="droppable-area-3" />
@@ -91,6 +81,30 @@ export default function Conteudo() {
                 <DroppableArea id="6" className="droppable-area-6" />
                 <DroppableArea id="7" className="droppable-area-7" />
               </div>
+              <div className='drop3'>
+                
+            {listaHorarios.map((horario, index) => (
+                  <div>
+                    <p className='horarios' style={{ textAlign: "center" }} key={index}>{horario}</p>
+                    <DroppableArea id={index} className={`droppable-area-${index} droppable-area`} />
+
+                  </div>
+
+                ))}
+                {/* <DroppableArea id="8" className="droppable-area-8" />
+                <DroppableArea id="9" className="droppable-area-9" />
+                <DroppableArea id="10" className="droppable-area-10" />
+                <DroppableArea id="11" className="droppable-area-11" />
+                <DroppableArea id="12" className="droppable-area-12" />
+                <DroppableArea id="13" className="droppable-area-13" />
+                <DroppableArea id="14" className="droppable-area-14" /> */}
+              </div>
+
+              {/* </div> */}
+              {/* 
+          <div className='buttonreset'> */}
+              {/* <div className='alinhamento_drop'> */}
+              
             </div>
 
 
