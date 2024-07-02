8fb25d49e211d413831348b60c60d127f20a7bef
Christian Rui <chris2w0social@gmail.com>
2024-07-02 01:44:20 -0300

    Arrumando Carrosel / arrumando modal do perfil

--- src/pages/loja/Loja.js
+++ src/pages/loja/Loja.js
@@ -24,92 +24,92 @@ const Loja = () => {
       <main className="loja-container">
         <div className="loja-background"></div>
         <div className='barras'>
-        <img src={barra} alt="barra" id='barra-energia' />
-      <img src={maisbuildolars} alt="maisbuildolars" />
-      </div>
+          <img src={barra} alt="barra" id='barra-energia' />
+          <img src={maisbuildolars} alt="maisbuildolars" />
+        </div>
         <h1 className="loja-title">{t("customization")}</h1>
 
         <div className="avatar-section">
           <h2 className="avatar-title">{t("avatarsForYou")}</h2>
           <div className="avatar-list">
-          <Carousel
-          customRightArrow={<CustomArrow props={"arrow"}></CustomArrow>}
-          customLeftArrow={<CustomArrow props={"arrow left loja"}></CustomArrow>}     
-          additionalTransfrom={100}
-          arrows
-          autoPlaySpeed={3000}
-          centerMode={false}
-          className=""
-          containerClass="container-with-dots containerLoja"
-          dotListClass=""
-          draggable
-          focusOnSelect={false}
-          infinite
-          itemClass="liCarroselLoja"
-          keyBoardControl={false}
-          minimumTouchDrag={80}
-          pauseOnHover
-          renderButtonGroupOutside={false}
-          renderDotsOutside={false}
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 4000,
-                min: 1024
-              },
-              items: 3,
-              partialVisibilityGutter: 50
-            },
-            mobile: {
-              breakpoint: {
-                max: 464,
-                min: 0
-              },
-              items: 1,
-              partialVisibilityGutter: 30
-            },
-            tablet: {
-              breakpoint: {
-                max: 1024,
-                min: 464
-              },
-              items: 2,
-              partialVisibilityGutter: 30
-            }
-          }}
-          rewind={false}
-          rewindWithAnimation={false}
-          rtl={false}
-          shouldResetAutoplay
-          showDots={false}
-          sliderClass="ulCarroselLoja"
-          slidesToSlide={1}
-          swipeable
-          
-        >
-          <div className="avatar-item">
-              <img src={skin1} alt="Avatar 1" />
-              <img src={buildolars} alt="Buildolars" className="buildolars" />
-              <p>550</p>
-            </div>
-            <div className="avatar-item">
-              <img src={skin2} alt="Avatar 2" />
-              <img src={buildolars} alt="Buildolars" className="buildolars" />
-              <p>760</p>
-            </div>
-            <div className="avatar-item">
-              <img src={skin3} alt="Avatar 3" />
-              <img src={buildolars} alt="Buildolars" className="buildolars" />
-              <p>990</p>
-            </div>
-        </Carousel>
+            <Carousel
+              customRightArrow={<CustomArrow props={"arrow loja"}></CustomArrow>}
+              customLeftArrow={<CustomArrow props={"arrow left loja"}></CustomArrow>}
+              additionalTransfrom={0}
+              arrows
+              autoPlaySpeed={3000}
+              centerMode={false}
+              className="carroselLoja"
+              containerClass="container-with-dots containerLoja"
+              dotListClass=""
+              draggable
+              focusOnSelect={false}
+              infinite
+              itemClass="liCarroselLoja"
+              keyBoardControl={false}
+              minimumTouchDrag={80}
+              pauseOnHover
+              renderButtonGroupOutside={false}
+              renderDotsOutside={false}
+              responsive={{
+                desktop: {
+                  breakpoint: {
+                    max: 4000,
+                    min: 1024
+                  },
+                  items: 3,
+                  partialVisibilityGutter: 50
+                },
+                mobile: {
+                  breakpoint: {
+                    max: 464,
+                    min: 0
+                  },
+                  items: 1,
+                  partialVisibilityGutter: 30
+                },
+                tablet: {
+                  breakpoint: {
+                    max: 1024,
+                    min: 464
+                  },
+                  items: 2,
+                  partialVisibilityGutter: 30
+                }
+              }}
+              rewind={false}
+              rewindWithAnimation={false}
+              rtl={false}
+              shouldResetAutoplay
+              showDots={false}
+              sliderClass="ulCarroselLoja"
+              slidesToSlide={1}
+              swipeable
+
+            >
+              <div className="avatar-item">
+                <img src={skin1} alt="Avatar 1" />
+                <img src={buildolars} alt="Buildolars" className="buildolars" />
+                <p>550</p>
+              </div>
+              <div className="avatar-item">
+                <img src={skin2} alt="Avatar 2" />
+                <img src={buildolars} alt="Buildolars" className="buildolars" />
+                <p>760</p>
+              </div>
+              <div className="avatar-item">
+                <img src={skin3} alt="Avatar 3" />
+                <img src={buildolars} alt="Buildolars" className="buildolars" />
+                <p>990</p>
+              </div>
+            </Carousel>
           </div>
         </div>
         <img src={listras} alt="listras" className="listras" />
 
         <div className="energia-section">
-        
-          
+
+
           <h2 className="energia-title">{t("nonStopEnergy")}</h2>
           <div className="energia-list">
             <div className="energia-group">
@@ -200,16 +200,16 @@ const Loja = () => {
         <img src={fundoroxo} alt="fundoroxo" className="fundoroxo" />
 
         <div className="missoes-section">
-        
-         
+
+
           <div className="desafio-item">
-          <h2 className="missoes-title">{t("challenges")}</h2>
-            
+            <h2 className="missoes-title">{t("challenges")}</h2>
+
             <div className="desafio-texto">
               <h3>{t("noTimeBro")}</h3>
               <p>
                 {t("spendLessEnergy")}
-                <br/>
+                <br />
                 {tComControleDeLinha(t("readyToActUnderPressure"))}
               </p>
             </div>
