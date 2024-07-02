c6d90a4d5830d0711264565e0edda579e2abb0f9
Gustavo Bosak Santos <gustavobosaksantos@gmail.com>
2024-07-01 23:39:05 -0300

    Atualização de responsividade da área de descanso

--- src/pages/areaDescanso/AreaDescanso.js
+++ src/pages/areaDescanso/AreaDescanso.js
@@ -9,41 +9,43 @@ export default function AreaDescanso() {
     const { t, tComControleDeLinha } = useControleDeTraducao();
 
     return (
-        <main id="areaDescanso">
-            <HeaderHome/>
-            <section className='titleTopFrame'>
-                <h1>{t("restQuestion")}</h1>
-            </section>
-            <section>
-                <a href='https://game-descanso-buildar.vercel.app/'>
-                    <article id='trexGame'>
+        <div>
+            <HeaderHome />
+            <main id="areaDescanso">
+                <section className='titleTopFrame'>
+                    <h1>{t("restQuestion")}</h1>
+                </section>
+                <section>
+                    <a href='https://game-descanso-buildar.vercel.app/'>
+                        <article id='trexGame'>
+                            <span></span>
+                            <div>
+                                <h3>{t("runViviRun")}</h3>
+                                <p>{tComControleDeLinha(t("runViviDescription"))}</p>
+                            </div>
+                        </article>
+                    </a>
+                    <Link to='/quiz'>
+                        <article id='quizGame'>
+                            <span></span>
+                            <div>
+                                <h3>{t("capQuiz")}</h3>
+                                <p>{tComControleDeLinha(t("capQuizDescription"))}</p>
+                            </div>
+                        </article>
+                    </Link>
+                    <article className='onDoing'>
                         <span></span>
                         <div>
-                            <h3>{t("runViviRun")}</h3>
-                            <p>{tComControleDeLinha(t("runViviDescription"))}</p>
+                            <h3>{t("gameUnderConstruction")}</h3>
+                            <p>{tComControleDeLinha(t("gameUnderConstructionDescription"))}</p>
                         </div>
                     </article>
-                </a>
-                <Link to='/quiz'>
-                    <article id='quizGame'>
-                        <span></span>
-                        <div>
-                            <h3>{t("capQuiz")}</h3>
-                            <p>{tComControleDeLinha(t("capQuizDescription"))}</p>
-                        </div>
-                    </article>
-                </Link>
-                <article className='onDoing'>
-                    <span></span>
-                    <div>
-                        <h3>{t("gameUnderConstruction")}</h3>
-                        <p>{tComControleDeLinha(t("gameUnderConstructionDescription"))}</p>
-                    </div>
-                </article>
-            </section>
-            <h4 id='moreSign'>{t("moreGamesComing")}</h4>
-            <Footer/>
-        </main>
+                </section>
+                <h4 id='moreSign'>{t("moreGamesComing")}</h4>
+            </main>
+            <Footer />
+        </div>
 
     )
 }
