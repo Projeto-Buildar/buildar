9e13637a1f7fd4b51fd015ae814dd435d63bb0a9
Christian-Rui <chris2w0social@gmail.com>
2024-11-15 20:57:25 -0300

    fix: arrumar imports errados das páginas da landingPage

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -1,9 +1,9 @@
 import './LandingPage.css';
 import Header from '../headersEfooters/Header/Header';
-import PrimeiraTela from './components/Inicio/PrimeiraTela';
-import SegundaTela from './components/Demonstracao/SegundaTela';
-import TerceiraTela from './components/Avatares/TerceiraTela';
-import QuartaTela from './components/Acessivel/QuartaTela';
+import PrimeiraTela from './components/Section1_Inicio/PrimeiraTela';
+import SegundaTela from './components/Section2_Demonstracao/SegundaTela';
+import TerceiraTela from './components/Section3_Avatares/TerceiraTela';
+import QuartaTela from './components/Section4_Acessivel/QuartaTela';
 import QuintaTela from './components/VejaMais/QuintaTela';
 import SextaTela from './components/Planos/SextaTela';
 import SetimaTela from './components/Eventos/SetimaTela';
