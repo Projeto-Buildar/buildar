db5ec3577fa9b0cfb3b01866e7082171a3cc976a
Christian-Rui <chris2w0social@gmail.com>
2024-11-15 21:36:09 -0300

    refactor: confirmar movimentação do componente para nova pasta

---
+++ src/pages/landingPage/components/Section4_Acessivel/QuartaTela.js
@@ -0,0 +1,29 @@
+import './QuartaTela.css';
+// import image from '../../images/quartaTela_img.webp';
+import image from '../../images/Vivi_VectorAzul.webp'
+
+import useControleDeTraducao from '../../../../shared/useControleDeTraducao';
+
+export default function QuartaTela() {
+
+  const { t, tFormatado } = useControleDeTraducao();
+  return (
+    <div className='frames' id="quartaTela">
+
+      <div className='backgroundLeft'></div>
+
+        <article>
+          <h2>{t('accessiblePlatform')}</h2>
+          <p>{t('learningOption')} <span className='destaqAzul'>{t('free')}</span> {tFormatado(t('ifInterested'))}.</p>
+        </article>
+
+        <figure className='figuraVivi'>
+          <img alt='mascote da Buildar "Vivi", fazendo uma pose de apresentação' src={image} className='ViviApresenta' />
+        </figure>
+
+
+      <div className='backgroundRight'></div>
+
+    </div>
+  );
+}
