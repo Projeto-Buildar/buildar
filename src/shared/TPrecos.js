7f1b93f133e060589d6c25b406bc4476a3e0a785
Christian-Rui <chris2w0social@gmail.com>
2024-11-16 01:31:31 -0300

    feat: adicionar funcionalidade de TPrecos.js para conversão de preços traduzidos

---
+++ src/shared/TPrecos.js
@@ -0,0 +1,36 @@
+import i18n from 'i18next';
+
+// Função para obter o idioma atual
+const getCurrentLanguage = () => i18n.language;
+
+// Função para formatar o número
+const formatarPreco = (valor, pontoEVirgula) => {
+  const [valorInteiro, valorDecimal] = valor.toFixed(2).split('.');
+  const separadoMilhar = pontoEVirgula ? '.' : ',';
+  const  separadorDecimal = pontoEVirgula ? ',' : '.';
+  const inteiroFormatado = valorInteiro.replace(/\B(?=(\d{3})+(?!\d))/g, separadoMilhar);
+  return `${inteiroFormatado}${separadorDecimal}${valorDecimal}`;
+};
+
+const TPrecos = (valor) => {
+  const currentLanguage = getCurrentLanguage();
+
+  let valorFinal;
+  switch (currentLanguage) {
+    case 'pt':
+      valorFinal = formatarPreco(valor, true);
+      return `R$${valorFinal}`;
+    case 'en':
+      const mediaDoDolar = 0.17;
+      valorFinal = formatarPreco(valor * mediaDoDolar, false);
+      return `$${valorFinal}`;
+    case 'es':
+      const mediaDoBob = 1.18;
+      valorFinal = formatarPreco(valor * mediaDoBob, true);
+      return `Bs${valorFinal}`;
+    default:
+      return "$NaN";
+  }
+};
+
+export default TPrecos;
