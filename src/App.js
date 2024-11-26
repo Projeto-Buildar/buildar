750fc111e76e02f4c20b6ee2b4d52997a8003ad8
Christian-Rui <chris2w0social@gmail.com>
2024-11-26 01:54:16 -0300

    feat: criar componente personalizado para gerenciamento de rotas
    o componente Rotas.js está na pastas 'routes' e agora realiza o gerenciamento de todas as rotas da aplicação, para que tanto a manutenção quanto a legibilidade do codigo fique melhorada e mais otimizada

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import './i18n';
 
 // Imports de Bibliotecas
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 
@@ -18,64 +17,11 @@ import { useTranslation } from 'react-i18next';
 em toda a árvore de componentes, sem a necessidade de passar props manualmente em cada nível.*/
 import { ItemProvider } from './pages/conteudo/Itens';
 
-// Imports de components
-//---------------------------------------------------------------|
-
-// Components acessiveis da landingPage
-import LandingPage from './pages/landingPage/LandingPage';
-import Login from './pages/entrada/Login';
-import Cadastro from './pages/entrada/Cadastro';
-import SobreNosTentativa from './pages/sobreTentativa/SobreNos';
-import FaleConosco from './pages/faleConosco/FaleConosco';
-import Termos from './pages/termos/Termos';
-
-// Components acessiveis do Home
-import Home from './pages/home/Home';
-import Corredor from './pages/corredor/Corredor';
-import Pratica from './pages/conteudo/Pratica';
-import Conteudos from './pages/conteudo/Conteudos';
-import Conteudo6 from './pages/conteudo/Conteudo6';
-import Conteudo7 from './pages/conteudo/Conteudo7';
-
-// Components acessiveis de Conexoes
-import Conexoes from './pages/conexoes/Conexoes';
-import Voluntario from './pages/inscricaoVoluntario/Voluntario';
-
-// Components acessiveis da Loja
-import Loja from './pages/loja/Loja';
-import Pagamento from './pages/pagamento/Pagamento';
-
-// Componente do Perfil
-import Perfil from './pages/perfil/Perfil';
-
-// Components acessiveis da Area de Descanso
-import AreaDescanso from './pages/areaDescanso/AreaDescanso';
-import GameDescanso from './GameDescanso';
-import InicioQuiz from './pages/quiz/InicioQuiz'
-import Quiz from './pages/quiz/Quiz1';
-import Quiz2 from './pages/quiz/Quiz2';
-
-//---------------------------------------------------------------|
-
-//Nao esta sendo utilizado
-//----------------------------------------------------|
-
-import Ranqueada from './pages/ranqueada/Ranqueada';
-
-// import SobreNos from './pages/sobre/SobreNos';
-// import LojaCopy from './pages/loja copy/LojaCopy';
-
-// import Conteudo from './pages/conteudo/Conteudo';
-// import Conteudo2 from './pages/conteudo/Conteudo2';
-// import Conteudo3 from './pages/conteudo/Conteudo3';
-// import Conteudo4 from './pages/conteudo/Conteudo4';
-// import Conteudo5 from './pages/conteudo/Conteudo5';
-
-//----------------------------------------------------|
+import Rotas from './routes/Rotas';
 
 export default function App() {
   const { i18n } = useTranslation();
-  
+
   // Inicializa o idioma selecionado com o valor armazenado no localStorage ou o padrão
   const [selectedLanguage, setSelectedLanguage] = useState(localStorage.getItem('language') || 'pt');
 
@@ -93,51 +39,16 @@ export default function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <DndProvider backend={HTML5Backend}>
-          <ItemProvider>
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="login" element={<Login />} />
-                <Route path="cadastro" element={<Cadastro />} />
-                <Route path="sobre" element={<SobreNosTentativa/>} />
-                <Route path="faleConosco" element={<FaleConosco />} />
-                <Route path='termos' element={<Termos></Termos>}/>
-                <Route path="home" element={<Home />} />
-                <Route path="corredor/:mapaNome" element={<Corredor />} />
-                <Route path='pratica' element={<Pratica />}></Route>
-                <Route path="conteudo6" element={<Conteudo6 />} />
-                <Route path="conteudos/:miniGame/conteudo6" element={<Conteudo6 />} />
-                <Route path="conteudos/:miniGame/conteudo7" element={<Conteudo7 />} />
-                <Route path='conteudos/:indice' element={<Conteudos />} />
-                <Route path="conexoes" element={<Conexoes />} />
-                <Route path="inscricaoVoluntario" element={<Voluntario />} />
-                <Route path="loja" element={<Loja />} />
-                <Route path="pagamento" element={<Pagamento />} />
-                <Route path="perfil" element={<Perfil />} />
-                <Route path='game_descanso' element={<GameDescanso />} />
-                <Route path='descanso' element={<AreaDescanso />}></Route>
-                <Route path="quiz" element={<InicioQuiz />} />
-                <Route path="quiz1" element={<Quiz />} />
-                <Route path="quiz2" element={<Quiz2 />} />
-                <Route path="ranqueada" element={<Ranqueada />} />
-                {/*           
-                  <Route path="conteudo" element={<Conteudo />} />
-                  <Route path="conteudo2" element={<Conteudo2 />} />
-                  <Route path="conteudo3" element={<Conteudo3 />} />
-                  <Route path="conteudo4" element={<Conteudo4 />} />
-                  <Route path="conteudo5" element={<Conteudo5 />} /> 
-                  <Route path="loja" element={<LojaCopy />} />
-                  <Route path='sobre_nos_1' element={<SobreNosTentativa/>} />
-                */}
-              </Routes>
-            </ItemProvider>
-          </DndProvider>
-      </BrowserRouter>
+
+      <DndProvider backend={HTML5Backend}>
+        <ItemProvider>
+          <Rotas />
+        </ItemProvider>
+      </DndProvider>
 
       <div className={`languageButton ${isFirefox ? '' : 'fontEmoji'}`}>
 
-        <select name="language" id="language" aria-label="Idioma"  onChange={handleLanguageChange} value={selectedLanguage}>
+        <select name="language" id="language" aria-label="Idioma" onChange={handleLanguageChange} value={selectedLanguage}>
 
           <option value="pt">🇧🇷</option>
           <option value="en">🇺🇸</option>
---
+++ src/routes/Rotas.js
@@ -0,0 +1,155 @@
+/* imports dos componentes funcionais da biblioteca React-Router-Dom 
+usado para definir rotas de navegação entre páginas*/
+//
+// - BrowserRouter: Envolve a aplicação inteira e fornece o contexto para o roteamento.
+// - Routes: Contém um conjunto de rotas definidas para a aplicação.
+// - Route: Define uma rota individual, especificando o caminho (path) e o componente a ser renderizado
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Imports dos components funcionais das páginas
+//---------------------------------------------------------------|
+
+// Components acessiveis da landingPage
+import LandingPage from '../pages/landingPage/LandingPage.js';
+import Login from '../pages/entrada/Login.js';
+import Cadastro from '../pages/entrada/Cadastro.js';
+import SobreNosTentativa from '../pages/sobreTentativa/SobreNos.js';
+import FaleConosco from '../pages/faleConosco/FaleConosco.js';
+import Termos from '../pages/termos/Termos.js';
+
+// Components acessiveis do Home
+import Home from '../pages/home/Home.js';
+import Corredor from '../pages/corredor/Corredor.js';
+import Pratica from '../pages/conteudo/Pratica.js';
+import Conteudos from '../pages/conteudo/Conteudos.js';
+import Conteudo6 from '../pages/conteudo/Conteudo6.js';
+import Conteudo7 from '../pages/conteudo/Conteudo7.js';
+
+// Components acessiveis de Conexoes
+import Conexoes from '../pages/conexoes/Conexoes.js';
+import Voluntario from '../pages/inscricaoVoluntario/Voluntario.js';
+
+// Components acessiveis da Loja
+import Loja from '../pages/loja/Loja.js';
+import Pagamento from '../pages/pagamento/Pagamento.js';
+
+// Componente do Perfil
+import Perfil from '../pages/perfil/Perfil.js';
+
+// Components acessiveis da Area de Descanso
+import AreaDescanso from '../pages/areaDescanso/AreaDescanso.js';
+import GameDescanso from '../GameDescanso.js';
+import InicioQuiz from '../pages/quiz/InicioQuiz.js'
+import Quiz from '../pages/quiz/Quiz1.js';
+import Quiz2 from '../pages/quiz/Quiz2.js';
+
+//---------------------------------------------------------------|
+
+export default function Rotas() {
+
+    const propriedadesRotas = [
+        {
+            path: "/",
+            element: < LandingPage />
+        },
+        {
+            path: "login",
+            element: < Login />
+        },
+        {
+            path: "cadastro",
+            element: < Cadastro />
+        },
+        {
+            path: "sobre",
+            element: < SobreNosTentativa />
+        },
+        {
+            path: "faleConosco",
+            element: < FaleConosco />
+        },
+        {
+            path: "termos",
+            element: < Termos />
+        },
+        {
+            path: "home",
+            element: < Home />
+        },
+        {
+            path: "corredor/:mapaNome",
+            element: < Corredor />
+        },
+        {
+            path: "pratica",
+            element: < Pratica />
+        },
+        {
+            path: "conteudo6",
+            element: < Conteudo6 />
+        },
+        {
+            path: "conteudos/:miniGame/conteudo6",
+            element: < Conteudo6 />
+        },
+        {
+            path: "conteudos/:miniGame/conteudo7",
+            element: < Conteudo7 />
+        },
+        {
+            path: "conteudos/:indice",
+            element: < Conteudos />
+        },
+        {
+            path: "conexoes",
+            element: < Conexoes />
+        },
+        {
+            path: "inscricaoVoluntario",
+            element: < Voluntario />
+        },
+        {
+            path: "loja",
+            element: < Loja />
+        },
+        {
+            path: "pagamento",
+            element: < Pagamento />
+        },
+        {
+            path: "perfil",
+            element: < Perfil />
+        },
+        {
+            path: "game_descanso",
+            element: < GameDescanso />
+        },
+        {
+            path: "descanso",
+            element: < AreaDescanso />
+        },
+        {
+            path: "quiz",
+            element: < InicioQuiz />
+        },
+        {
+            path: "quiz1",
+            element: < Quiz />
+        },
+        {
+            path: "quiz2",
+            element: < Quiz2 />
+        }
+    ]
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                {propriedadesRotas.map((propriedades) => {
+                    return <Route key={propriedades.path} {...propriedades} />
+                })}
+            </Routes>
+        </BrowserRouter>
+    );
+
+}
