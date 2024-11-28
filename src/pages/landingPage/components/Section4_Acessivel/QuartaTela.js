beb239a6844458525169f0b6a9eb230d375d94d0
Christian-Rui <chris2w0social@gmail.com>
2024-11-28 01:30:20 -0300

    style: criar responsividade da section4 da landingPage

--- src/pages/landingPage/components/Section4_Acessivel/QuartaTela.js
+++ src/pages/landingPage/components/Section4_Acessivel/QuartaTela.js
@@ -1,40 +1,41 @@
-import './QuartaTela.css';
-// import image from '../../images/quartaTela_img.webp';
+import useControleDeTraducao from '../../../../shared/useControleDeTraducao';
+
+import { TranslationTagsComPrefixo as TComTag } from '../../../../shared/TranslationTagsComPrefixo';
+
 import image from '../../images/Vivi_VectorAzul.webp'
 
-import useControleDeTraducao from '../../../../shared/useControleDeTraducao';
-import { TranslationTagsComPrefixo as TComTag } from '../../../../shared/TranslationTagsComPrefixo';
+import './QuartaTela.css';
 
 export default function QuartaTela() {
 
   const { chaveComPrefixo } = useControleDeTraducao("section4");
   return (
-    <div className='frames' id="quartaTela">
+    <section className='frames' id="quartaTela">
+      <article className='section4'>
 
-      <div className='backgroundLeft'></div>
+        <div className='blocoDeTexto'>
+          <h2>
+            <TComTag
+              i18nKey={chaveComPrefixo("subtitulo")}
+              components={{ span: <span className='destaqueRoxo' /> }}
+            />
+          </h2>
+          <p>
+            <TComTag
+              i18nKey={chaveComPrefixo("descricao")}
+              components={{ span: <span className='destaqVermelho' /> }}
+            />
+          </p>
+        </div>
 
-      <article>
-        <h2>
-          <TComTag
-            i18nKey={chaveComPrefixo("subtitulo")}
-            components={{ span: <span className='destaqueRoxo' /> }}
-          />
-        </h2>
-        <p>
-          <TComTag
-            i18nKey={chaveComPrefixo("descricao")}
-            components={{ span: <span className='destaqVermelho' /> }}
-          />
-        </p>
+        <figure className='figuraVivi'>
+          <img alt='mascote da Buildar "Vivi", fazendo uma pose de apresentação' src={image} className='ViviApresenta' />
+        </figure>
+        
       </article>
 
-      <figure className='figuraVivi'>
-        <img alt='mascote da Buildar "Vivi", fazendo uma pose de apresentação' src={image} className='ViviApresenta' />
-      </figure>
-
-
       <div className='backgroundRight'></div>
-
-    </div>
+      <div className='backgroundLeft'></div>
+    </section>
   );
 }
