710a8c868aadb93af17c46149ded51cf5c3f9c46
Christian-Rui <chris2w0social@gmail.com>
2024-11-27 22:06:41 -0300

    style: criar responsividade para section2 da landingPage

--- src/pages/landingPage/components/Section2_Demonstracao/SegundaTela.js
+++ src/pages/landingPage/components/Section2_Demonstracao/SegundaTela.js
@@ -7,28 +7,31 @@ export default function SegundaTela() {
   const { chaveComPrefixo } = useControleDeTraducao("section2");
   // // Função que aplica o prefixo automaticamente
   // const chaveComPrefixo = usePrefixo("Section2")
-  
+
   return (
-    <div className='frames' id="segundaTela">
-      <article>
-        <h2>
-          <TComTag
+    <section className='frames' id="segundaTela">
+      <article className='section2'>
+        <div className='blocoDeTexto'>
+          <h2>
+            <TComTag
               i18nKey={chaveComPrefixo("subtitulo")}
               components={{ span: <span className='destaqueRoxo' /> }}
             />
-        </h2>
-        <p>
-          <TComTag
+          </h2>
+          <p>
+            <TComTag
               i18nKey={chaveComPrefixo("descricao")}
               components={{ span: <span className='destaqVermelho' /> }}
-          />
-        </p>
-      </article>
-      <section>
-        <div className='monitor'>
-        <img src={gif} alt='Monitor'/>
+            />
+          </p>
         </div>
-      </section>
-    </div>
+
+        <figure>
+          <div className='monitor'>
+            <img src={gif} alt='Monitor' />
+          </div>
+        </figure>
+      </article>
+    </section>
   );
 }
